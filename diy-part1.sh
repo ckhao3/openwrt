1139a7f2929ea4624cc1c61b40f460fb3060dd38
ckhao <97717542+ckhao3@users.noreply.github.com>
2025-04-01 18:30:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,15 @@
 # See /LICENSE for more information.
 #
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # Add a feed source
 git clone --depth=1 https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns.git package/luci-app-tencentddns
